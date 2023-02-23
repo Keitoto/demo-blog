0ee63a696323d511e7804b0e0ca1107dd04def2d
Keitoto <keitasekihara@gmail.com>
2023-02-23 15:09:53 +0900

    ✨ added provider for redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import '@/index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from '@/App';
+import { store } from '@/app/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
