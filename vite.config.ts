02a2789414c9ef27576f5528b3acedbe7e484e67
Keitoto <keitasekihara@gmail.com>
2023-02-23 16:28:17 +0900

    🧑‍💻 changed dev port to localhost:3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
+  },
+  server: {
+    host: 'localhost',
+    port: 3000,
   },
 });
