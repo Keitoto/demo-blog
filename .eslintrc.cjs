6d55593efe5b6384d7b1c6530e06ab5d0f7c4764
Keitoto <keitasekihara@gmail.com>
2023-02-23 14:58:57 +0900

    🎉 Initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'sort-keys-custom-order',
+    'simple-import-sort',
+    '@typescript-eslint',
+  ],
+  ignorePatterns: ['vite.config.ts', '.eslintrc.cjs'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'simple-import-sort/imports': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'arrow-body-style': 0, // change to ["error", "always"] before build
+    'react/function-component-definition': [
+      //アロー関数以外受け付けない設定
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state',
+      ]
+    }],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+
+const App = () => {
+  return <div className="App">Hi</div>;
+};
+
+export default App;
